355af9aa721f1424c41739c8658aa2aa1ffc7387
Julien Cayzac <julien.cayzac@gmail.com>
2024-08-16 22:01:29 +0900

    fix docs

--- astro-classlist/index.ts
+++ astro-classlist/index.ts
@@ -1,37 +1,54 @@
 import type { AstroBuiltinAttributes } from 'astro'
 
+/**
+ * The type of the `class:list` attribute.
+ */
 export type ClassList = AstroBuiltinAttributes['class:list']
 
 /**
- * Convert an astro `class:list` expression to an array of strings.
- * This is useful when you want to accept a `class:list` prop and pass it
- * to a component, while adding your own classes too.
+ * A utility object for working with the `class:list` attribute.
  */
-function toArray(classList: ClassList): string[] {
-	if (typeof classList === 'string') {
-		return [classList]
-	}
-
-	if (Array.isArray(classList)) {
-		return classList
-	}
-
-	if (classList !== null || classList !== undefined) {
-		if ((classList as any)[Symbol.iterator] !== undefined) {
-			return [...(classList as Iterable<any>)].map(classList => classList.toString())
-		}
-
-		if (typeof classList === 'object') {
-			return Object.entries(classList)
-				.filter(([_, v]) => !!v)
-				.map(([k]) => k)
-		}
-	}
-
-	return []
+export interface ClassListStaticFunctions {
+	/**
+	 * Converts an astro `class:list` expression to an array of strings.
+	 * This is useful when you want to accept a `class:list` prop and pass it
+	 * to a component, while adding your own classes too.
+	 */
+	toArray(classList: ClassList): string[]
 }
 
+
+/**
+ * A utility object for working with the `class:list` attribute.
+ */
 // eslint-disable-next-line ts/no-redeclare
-export const ClassList = {
-	toArray,
+export const ClassList: ClassListStaticFunctions = {
+	/**
+	 * Converts an astro `class:list` expression to an array of strings.
+	 * This is useful when you want to accept a `class:list` prop and pass it
+	 * to a component, while adding your own classes too.
+	 */
+	toArray(classList: ClassList): string[] {
+		if (typeof classList === 'string') {
+			return [classList]
+		}
+	
+		if (Array.isArray(classList)) {
+			return classList
+		}
+	
+		if (classList !== null || classList !== undefined) {
+			if ((classList as any)[Symbol.iterator] !== undefined) {
+				return [...(classList as Iterable<any>)].map(classList => classList.toString())
+			}
+	
+			if (typeof classList === 'object') {
+				return Object.entries(classList)
+					.filter(([_, v]) => !!v)
+					.map(([k]) => k)
+			}
+		}
+	
+		return []
+	}	
 }
