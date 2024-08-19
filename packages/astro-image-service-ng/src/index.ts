c1c7543e95d44dd96fccf66aa7876b4da0d1e678
Julien Cayzac <julien.cayzac@gmail.com>
2024-08-19 18:07:11 +0900

    no external

--- packages/astro-image-service-ng/src/index.ts
+++ packages/astro-image-service-ng/src/index.ts
@@ -5,52 +5,13 @@ function integration(): AstroIntegration {
 		name: '@jcayzac/astro-image-service-ng/service',
 		hooks: {
 			'astro:config:setup': (options) => {
-				const { config, updateConfig } = options
-
-				let external = config.vite?.build?.rollupOptions?.external
-
+				const { updateConfig } = options
 				updateConfig({
 					image: {
 						service: {
 							entrypoint: '@jcayzac/astro-image-service-ng/service',
 							config: {
 								debug: true,
-							},
-						},
-					},
-					vite: {
-						...(config.vite || {}),
-						build: {
-							...(config.vite?.build || {}),
-							rollupOptions: {
-								...(config.vite?.build?.rollupOptions || {}),
-								external: (source: string, importer: string | undefined, isResolved: boolean) => {
-									if (source === '@jcayzac/astro-image-service-ng/service') {
-										return true
-									}
-
-									if (external === undefined) {
-										return false
-									}
-
-									if (typeof external === 'function') {
-										return external(source, importer, isResolved)
-									}
-
-									if (!Array.isArray(external)) {
-										external = [external]
-									}
-
-									for (const e of external) {
-										if (typeof e === 'string' && source === e) {
-											return true
-										}
-
-										if (e instanceof RegExp && e.test(source)) {
-											return true
-										}
-									}
-								},
 							},
 						},
 					},
