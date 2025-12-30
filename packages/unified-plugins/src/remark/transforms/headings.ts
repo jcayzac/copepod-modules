dcbc5477b75e43788406531e643cbaeea294ee90
Julien Cayzac <julien.cayzac@rakuten.com>
2025-12-30 21:26:21 +0900

    the baseline plugin doesn't transform headings anymore

--- packages/unified-plugins/src/remark/transforms/headings.ts
+++ packages/unified-plugins/src/remark/transforms/headings.ts
@@ -5,6 +5,10 @@ export interface Slugger {
 	slug: (value: string) => string
 }
 
+/**
+ * Wrap a heading's content in a link to itself and add an ID using the provided slugger.
+ * @note This is not included anymore in the baseline plugin by default.
+ */
 export function headings(parent: Parent, slugger: Slugger) {
 	if (parent.type !== 'heading' || !parent.children[0]) {
 		return
