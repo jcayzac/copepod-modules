c469f748511104967ec4f78276cadbdfd2a47ed1
Julien Cayzac <julien.cayzac@rakuten.com>
2026-01-02 16:29:27 +0900

    fix breakpoints not being passed to components

--- packages/unified-plugins/src/remark/fences.ts
+++ packages/unified-plugins/src/remark/fences.ts
@@ -1,8 +1,12 @@
-/*
- Route code blocks to components based on the 'language' attribute.
+/**
+ * @file Process fenced code blocks in Markdown files.
+ *
+ * 1. Route code blocks to components based on the 'language' attribute.
+ * 2. Add breakpoints based on the maximum line length.
+ * 3. Route unregistered languages to a default component if specified.
  */
 import type { Code, Parent, Root } from 'mdast'
-import type { MdxJsxAttribute, MdxJsxFlowElement } from 'mdast-util-mdx-jsx'
+import type { MdxJsxAttribute, MdxJsxAttributeValueExpression, MdxJsxFlowElement } from 'mdast-util-mdx-jsx'
 import { onlyParents } from './utils'
 
 export interface FencesOptions {
@@ -22,6 +26,27 @@ export interface FencesOptions {
 	readonly breakpoints?: [number, string][] | undefined
 }
 
+function makeElement(
+	code: Code,
+	component: string,
+	attributes: Record<string, MdxJsxAttributeValueExpression | string | null>,
+) {
+	return {
+		type: 'mdxJsxFlowElement',
+		name: component,
+		attributes: Object.entries(attributes).map(([name, value]) => ({
+			type: 'mdxJsxAttribute',
+			name,
+			value,
+		} satisfies MdxJsxAttribute)),
+		children: [],
+		position: code.position,
+		data: {
+			...code.data,
+		},
+	} as MdxJsxFlowElement
+}
+
 export function fences(options: FencesOptions = {}) {
 	const { defaultComponent } = options
 	const componentRoutes = options.componentRoutes ?? {}
@@ -31,6 +56,37 @@ export function fences(options: FencesOptions = {}) {
 		const queue: Array<Parent> = []
 		let parent: Parent | undefined
 
+		// First pass: Add breakpoints to code elements.
+		if (breakpoints.length) {
+			queue.push(root)
+			// eslint-disable-next-line no-cond-assign
+			while (parent = queue.shift()) {
+				for (const child of parent.children) {
+					if (child.type !== 'code') {
+						continue
+					}
+					const { data = {}, value } = child as Code
+
+					// Count characters per line and set breakpoints
+					const maxLength = value.split('\n').map(line => line.length).reduce((max, len) => Math.max(max, len), 0)
+					let match = 0
+					for (const [length, breakpoint] of breakpoints) {
+						if (maxLength >= length && length > match) {
+							match = length
+							child.data = {
+								...data,
+								hProperties: {
+									...data?.hProperties,
+									'data-breakpoint': breakpoint,
+								},
+							}
+						}
+					}
+				}
+				queue.push(...onlyParents(parent.children))
+			}
+		}
+
 		// First pass: Replace code blocks with components.
 		queue.push(root)
 		// eslint-disable-next-line no-cond-assign
@@ -39,7 +95,9 @@ export function fences(options: FencesOptions = {}) {
 				if (child.type !== 'code') {
 					continue
 				}
-				const { lang, meta, data: oldData = {}, value } = child as Code
+				const { lang, meta, value, data } = child as Code
+				const { hProperties = {} } = data ?? {}
+				const breakpoint = hProperties['data-breakpoint'] as string | null ?? null
 
 				// If no component is registered for the language, skip.
 				const component = lang ? componentRoutes[lang] : defaultComponent
@@ -54,61 +112,15 @@ export function fences(options: FencesOptions = {}) {
 					continue
 				}
 
-				const mdx = {
-					type: 'mdxJsxFlowElement',
-					name: component,
-					attributes: [
-						{
-							type: 'mdxJsxAttribute',
-							name: 'meta',
-							value: meta ?? '',
-						},
-						{
-							type: 'mdxJsxAttribute',
-							name: 'source',
-							value,
-						},
-					] satisfies MdxJsxAttribute[],
-					children: [],
-					position: child.position,
-					data: {
-						...oldData,
-					},
-				} as MdxJsxFlowElement
+				const mdx = makeElement(child, component, {
+					meta: meta ?? '',
+					source: value,
+					breakpoint,
+				})
 				parent.children[index] = mdx
 			}
 
 			queue.push(...onlyParents(parent.children))
-		}
-
-		// Second pass: Add breakpoints to code blocks.
-		if (breakpoints.length) {
-			queue.push(root)
-			// eslint-disable-next-line no-cond-assign
-			while (parent = queue.shift()) {
-				for (const child of parent.children) {
-					if (child.type !== 'code') {
-						continue
-					}
-					const { data = {}, value } = child as Code
-
-					// Count characters per line and set breakpoints
-					const maxLength = value.split('\n').map(line => line.length).reduce((max, len) => Math.max(max, len), 0)
-					for (const [length, breakpoint] of breakpoints) {
-						if (maxLength >= length) {
-							child.data = {
-								...data,
-								hProperties: {
-									...data?.hProperties,
-									'data-breakpoint': breakpoint,
-								},
-							}
-							break
-						}
-					}
-				}
-				queue.push(...onlyParents(parent.children))
-			}
 		}
 	}
 }
